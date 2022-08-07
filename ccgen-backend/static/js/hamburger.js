33686fccc2d72ae7fb457e8752e24e9d1179d7d8
Victoria <adedayovicky123@gmail.com>
2022-08-07 09:35:58 +0100

    revert back to previous file structure

--- ccgen-backend/static/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-const hamburgerButton = document.getElementById('hamburger-btn');
-
-const toggleHamburgerMenu = () => {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    
-    hamburgerMenu.classList.toggle('hide');
-}
-
-hamburgerButton.addEventListener('click', toggleHamburgerMenu);
