df16f28b784cf6cb697b262cc50bdc113fad53ac
SidKay <sidkay1234@gmail.com>
2022-08-16 19:54:27 +0100

    Linked to postgresql

--- ccgen/library/views.py
+++ ccgen/library/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import DesignInfo
+from django.http.response import Http404
 
 def library(request):
     designs = DesignInfo.objects.all()    
     return render(request, "library.html", {'designs':designs})
 
 def individual(request):
-    return render(request, "individual.html")
+    return render(request, "individual.html", {})
