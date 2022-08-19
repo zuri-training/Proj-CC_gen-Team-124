30f7b3a3a6733fb58eab13cfb84cce51a0cef7c6
Victoria <adedayovicky123@gmail.com>
2022-08-19 19:13:12 +0100

    update

--- ccgen/ccgen/settings.py
+++ ccgen/ccgen/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
