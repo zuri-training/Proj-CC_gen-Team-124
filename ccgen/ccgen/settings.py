4e7fc682aca2c319e74f8720e2b7c012a4c1de85
Victoria <adedayovicky123@gmail.com>
2022-08-19 19:18:10 +0100

    update

--- ccgen/ccgen/settings.py
+++ ccgen/ccgen/settings.py
@@ -90,9 +90,11 @@ WSGI_APPLICATION = 'ccgen.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': os.path.join(BASE_DIR, 'db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
